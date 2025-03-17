7eba2aff6d6c8a9c9c74b8e9669349b3fc4ac2ab
Qiu <JQiu@njohsp.gov>
2025-03-17 14:37:57 -0400

    update the background pic

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import businessCard from '../pics/businessCard.png';
 
 function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About US</h1>
       {/* Add your about page content here */}
+      <div className="business-card">
+          <img src={businessCard} alt="Business Card" />
+        </div>
     </div>
+    
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,35 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import '../styles/Home.css';
-import businessCard from '../pics/businessCard.png';
 
 function Home() {
   return (
-    <div>
-      <div className="home">
-        <div className="overlay"></div>
-        <div className="business-card">
-          <img src={businessCard} alt="Business Card" />
-        </div>
-        <div className='welcome'>
-          <h1>Your Trusted Real Estate Partner In Mid Jersey Area.</h1>
-        </div>
-        <div className="buttons">
-          <Button variant="contained" color="primary" component={Link} to="/buying">
-            I am buying
-          </Button>
-          <Button variant="contained" color="primary" component={Link} to="/selling">
-            I am selling
-          </Button>
-          <Button variant="contained" color="primary" component={Link} to="/renting">
-            I am renting
-          </Button>
-        </div>
-        <div className='office'>
-          <h4>Mobile: <a href="tel:+16095809749">(609) 580-9749</a></h4>
-          <h4>Email: <a href="mailto:joeqiu19@gmail.com">joeqiu19@gmail.com</a></h4>
-          <h4>Address: 856 US-206 Building B Suite 13-B, Hillsborough Township, NJ 08844</h4>
-        </div>
+    <div className="home">
+      <div className="overlay"></div>
+      <div className='welcome'>
+        <h1>Your Trusted Real Estate Partner</h1>
+        <h2>Serving Mid Jersey Area</h2>
+        <p>Save Time. Avoid Hassle.</p>
+        <p>Let Us Understand Your Needs!</p>
+      </div>
+      <div className="buttons">
+        <Button variant="contained" color="primary" component={Link} to="/buying">
+          I am buying
+        </Button>
+        <Button variant="contained" color="primary" component={Link} to="/selling">
+          I am selling
+        </Button>
+        <Button variant="contained" color="primary" component={Link} to="/renting">
+          I am renting
+        </Button>
+      </div>
+      <div className='office'>
+        <h4>Mobile: (609) 580-9749</h4>
+        <h4>Email: <a href="mailto:joeqiu19@gmail.com" className="email-link">joeqiu19@gmail.com</a></h4>
+        <h4>Address: 856 US-206 Building B Suite 13-B, Hillsborough Township, NJ 08844</h4>
       </div>
     </div>
   );
